3bb06b353207c9efc5480e4f45b935cca98b612f
andriusdev1 <rimkunas.andrius@gmail.com>
2023-03-01 21:49:31 +0000

    Still working on a fix

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const ticketPrice = +movieSelect.value;
 
 // Update total and count 
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected')
-    ;
+    const selectedSeats = document.querySelectorAll
+    ('.row .seat.selected');
 
     const selectedSeatsCount = selectedSeats.length;
 
-    count.innerText = selectedSeatsCount;
+  count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
