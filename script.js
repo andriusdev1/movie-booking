65695fd78908a32a0d202c74d323bd5b7508d0e3
andriusdev1 <rimkunas.andrius@gmail.com>
2023-03-04 21:46:20 +0000

    Saving data to local storage

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+
+populateUI()
+
 let ticketPrice = +movieSelect.value;
+
+
+
+
+//Save selected movie index and price
+function setMovieDate(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 
 
 // Update total and count 
@@ -16,15 +29,35 @@ const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
 localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
+
     const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+
+// Get data from localstorage and populate UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')
+  );
+
+if(selectedSeats !== null && selectedSeats.length > 0){
+seats.forEach((seat,index) =>  {
+  if(selectedSeats.indexOf(index) > -1) {
+seat.classList.add('selected');
+}
+});
+};
+
+
+
+
 //Movie select event
 movieSelect.addEventListener('change', e =>{
   ticketPrice = +e.target.value;
+  setMovieDate();
   updateSelectedCount();
 });
 
@@ -39,4 +72,4 @@ container.addEventListener('click', e => {
 
     updateSelectedCount();
  }
-});
+})};
